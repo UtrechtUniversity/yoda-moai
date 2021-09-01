bad3efd2f82dde0a91d585485b0230699f30fd08
Harm de Raaff <harm.apple@gmail.com>
2021-09-01 08:19:39 +0200

    fixed

--- moai/metadata/datacite.py
+++ moai/metadata/datacite.py
@@ -4,7 +4,6 @@ from lxml.builder import ElementMaker
 XSI_NS = 'http://www.w3.org/2001/XMLSchema-instance'
 XML_NS = 'https://www.w3.org/TR/xml-names/'
 
-
 class DataCite(object):
     """The standard Datacite.
 
@@ -17,7 +16,8 @@ class DataCite(object):
         self.db = db
 
         self.ns = {'datacite': 'http://datacite.org/schema/kernel-4',
-                   'oai_datacite': 'http://datacite.org/schema/kernel-4'}
+                   'oai_datacite': 'http://datacite.org/schema/kernel-4',
+                   'xml': XML_NS }
         self.schemas = {'datacite': 'http://schema.datacite.org/meta/kernel-4/metadata.xsd',
                         'oai_datacite': 'http://schema.datacite.org/meta/kernel-4/metadata.xsd'}
 
@@ -292,7 +292,16 @@ class DataCite(object):
         dictResourceTypes = {'Dataset': 'Research Data',
                              'DataPaper': 'Method Description',
                              'Software': 'Computer Code',
+                             'Model': 'Model',
                              'Text': 'Other Document'}
+
+        ''' teclab / hptlab  for now only Model has been added in the dict above
+        "Dataset": "Datapackage",
+        "DataPaper": "Method",
+        "Software": "Software",
+        "Model": "Model",
+        "Text": "Other document"
+        '''
 
         try:
             resourceTypeGeneral = data['Data_Type']
